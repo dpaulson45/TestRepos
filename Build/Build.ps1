7c7c55e5607d9e09464a4e7f6e1cac5ed2c299d8
David Paulson <dpaul@microsoft.com>
2020-07-10 22:48:15 -0500

    removed '.'

--- Build/Build.ps1
+++ Build/Build.ps1
@@ -13,6 +13,6 @@ $outputText = @"
 SHA256: {0}
 
 How to Verify Hash Value: https://github.com/dpaulson45/HealthChecker/wiki/How-to-Verify-Hash-Value
-"@ -f ((Get-FileHash -Algorithm SHA256 "..\Test-Script\TestScript.ps1").Hash)
+"@ -f ((Get-FileHash -Algorithm SHA256 ".\Test-Script\TestScript.ps1").Hash)
 
 $outputText | Out-File .\ReleaseNotes.txt
