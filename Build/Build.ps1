850b5890b6502486f49847c6cda89bfbea00d5e6
David Paulson <dpaul@microsoft.com>
2020-07-10 23:06:13 -0500

    Trying new Release Notes

--- Build/Build.ps1
+++ Build/Build.ps1
@@ -15,4 +15,4 @@ SHA256: {0}
 How to Verify Hash Value: https://github.com/dpaulson45/HealthChecker/wiki/How-to-Verify-Hash-Value
 "@ -f ((Get-FileHash -Algorithm SHA256 ".\Test-Script\TestScript.ps1").Hash)
 
-$outputText | Out-File .\ReleaseNotes.txt
+$outputText  > .\Build\ReleaseNotes.txt
