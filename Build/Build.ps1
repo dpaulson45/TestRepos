9eeac1f11739352817ae65507ced2353c284dd5b
David Paulson <dpaul@microsoft.com>
2020-07-10 22:44:17 -0500

    Testing Release Notes
    test

--- Build/Build.ps1
+++ Build/Build.ps1
@@ -1,7 +1,17 @@
 Write-Host "Hello World"
 
-if ($true)
+if ($false)
 {
     #testing failing only way to exit and get a fail is to do exit 1
     exit 1
 }
+
+#Create Release Notes
+
+$outputText = @"
+SHA256: {0}
+
+How to Verify Hash Value: https://github.com/dpaulson45/HealthChecker/wiki/How-to-Verify-Hash-Value
+"@ -f ((Get-FileHash -Algorithm SHA256 "..\Test-Script\TestScript.ps1").Hash)
+
+$outputText | Out-File .\ReleaseNotes.txt
